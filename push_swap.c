272e2d7ef108912c65fd6d5f7595ff918962cae6
hnagasak <hnagasak@student.42tokyo.jp>
2023-10-26 20:31:44 +0900

    update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -6,69 +6,55 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 05:57:37 by hnagasak          #+#    #+#             */
-/*   Updated: 2023/10/25 04:33:24 by hnagasak         ###   ########.fr       */
+/*   Updated: 2023/10/26 20:19:53 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	stack_test(void)
+int	*argvtoi(int argc, char *argv[])
 {
-	t_stack	*stack;
+	int	i;
+	int	*values;
 
-	stack = create_stack("stack_A");
-	printf("create %s Stack!!\n", stack->name);
-	push(stack, 111);
-	push(stack, 222);
-	push(stack, 333);
-	print_stack(stack);
-	printf("pop %d\n", pop(stack));
-	printf("pop %d\n", pop(stack));
-	push(stack, 444);
-	/// 444 111
-	printf("-----\n");
-	print_stack(stack);
-
-	printf("top:%p bottom:%p\n", stack->top, stack->bottom);
-	printf("pop %d\n", pop(stack));
-	printf("top:%p bottom:%p\n", stack->top, stack->bottom);
-	printf("pop %d\n", pop(stack));
-	printf("top:%p bottom:%p\n", stack->top, stack->bottom);
-	printf("pop %d\n", pop(stack));
-	printf("top:%p bottom:%p\n", stack->top, stack->bottom);
-	printf("-----\n");
-	print_stack(stack);
-
-	printf("top:%p bottom:%p\n", stack->top, stack->bottom);
-}
-
-void test(){
-
-	t_stack	*a;
-	t_stack	*b;
-
-	a = create_stack("Stack-A");
-	b = create_stack("Stack-B");
-	push(a, 111);
-	push(a, 222);
-	push(a, 333);
-	push(b, 444);
-	push(b, 555);
-	push(b, 666);
-	print_stack(a);
-	print_stack(b);
-	pb(a,b);
-	print_stack(a);
-	print_stack(b);
-
+	values = (int *)malloc(sizeof(int) * argc);
+	i = 0;
+	while (i < argc - 1)
+	{
+		values[i] = ft_atoi(argv[i + 1]);
+		// printf("values[%d]:%d\n", i, values[i]);
+		i++;
+	}
+	return (values);
 }
 
 int	main(int argc, char *argv[])
 {
-	size_t	size;
+	int		values_count;
+	int		*values;
+	t_stack	*stca;
+	t_stack	*stcb;
 
-	size = argc - 1;
-	printf("count:%zu\n", size);
-	printf("argv[1]:%s\n", argv[1]);
-	test();
+	// 入力取得
+	values_count = argc - 1;
+	// printf("values_count:%d argv[1]:%s\n", values_count, argv[1]);
+	values = argvtoi(argc, argv);
+	// printf("values[%d]:%d\n", values_count - 1, values[values_count - 1]);
+
+	// スタック初期化
+	stca = create_stack("stack_A", values, values_count);
+	stcb = create_stack("stack_B", NULL, 0);
+	print_stack(stca);
+	// print_stack(stcb);
+	if (values_count <= 3)
+		sort3(stca);
+
+	// ra(stca);
+	// if (stack_size == 6)
+	// 	sort6(values);
+	// else
+	// 	sort7(values);
+	print_stack(stca);
+	print_stack(stcb);
+	// test();
 }
